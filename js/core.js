c528e49ef38297c984d2d5c81f90091666c58055
assessoriadigitalizesites <assessoria.contato2025@gmail.com>
2025-02-04 06:58:59 -0300

    Add files via upload

---
+++ js/core.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  // var height = window.screen.width < 768 ? 30 : 100;
+  var height = 80;
+  $(window).scroll(function (event) {
+    if (window.scrollY > height) {
+      $(".header").addClass("header-scrolled");
+    } else {
+      $(".header").removeClass("header-scrolled");
+    }
+  });
+});
+
+$("body").on("click", ".btn, .blue-box-btn", function() {
+  window.open("https://api.whatsapp.com/send?phone=14073606377&text=Bem%20vindo%20!", "_blank");
+});
+
+$("body").on("click", ".header-menu-item", function() {
+  toggleMenu();
+  $("html, body").animate({ scrollTop: $($(this).attr("data-href")).offset().top - 80 });
+})
+
+$("body").on("click", ".header-menu-btn", function() {
+  toggleMenu();
+});
+
+function toggleMenu() {
+  $(".header-menu-btn").toggleClass("active");
+  $(".header-menu").toggleClass("show");
+}
